b43b616d6e1618d16692c7f75d14a9bd31cbcb48
Guilherme Garcia <garciaguig@gmail.com>
2022-06-08 23:24:03 -0300

    Conclusão do Requisito 18

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+// Declaração das Variáveis;
 const emailInput = document.querySelector('#email');
 const passwordInput = document.querySelector('#password');
-const submitButton = document.querySelector('#submit-button');
+const loginButton = document.querySelector('#submit-button');
+const agreementCheck = document.querySelector('#agreement');
+const submitButton = document.querySelector('#submit-btn');
 
+// Declaração das Funções;
 function validateLogin(event) {
   if (emailInput.value === 'tryber@teste.com' && passwordInput.value === '123456') {
     window.alert('Olá, Tryber!');
@@ -13,4 +17,17 @@ function validateLogin(event) {
   }
 }
 
-submitButton.addEventListener('click', validateLogin);
+// Ativação das Funções
+window.addEventListener('load', () => {
+  submitButton.disabled = true;
+});
+
+loginButton.addEventListener('click', validateLogin);
+
+agreementCheck.addEventListener('click', () => {
+  if (agreementCheck.checked === true) {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
+  }
+});
